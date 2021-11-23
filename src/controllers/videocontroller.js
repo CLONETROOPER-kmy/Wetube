1b476a0a96895a6895db20446d823e294996b041
CLONETROOPER-kmy <koreastarwarsclone@gmail.com>
2021-11-24 00:04:29 +0900

    templates

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,17 +1,39 @@
-export const hompage = (req, res) => res.send("Home Page Videos");
-
-export const watch = (req, res) => {
-  return res.send(`Watch videos #${req.params.id}`);
+export const homepage = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 10,
+      createAt: "5 seconds ago",
+      views: 100,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 10,
+      createAt: "5 seconds ago",
+      views: 100,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 10,
+      createAt: "5 seconds ago",
+      views: 100,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
 };
 
-export const edit = (req, res) => {
-  res.send("Eidt");
-};
+export const watch = (req, res) => res.render("watch");
+
+export const edit = (req, res) => res.send("Edit");
 
 export const search = (req, res) => res.send("Search");
 
 export const upload = (req, res) => res.send("Upload");
 
-export const removevideo = (req, res) => {
-  res.send("Remove video");
-};
+export const removevideo = (req, res) => res.send("Remove video");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { hompage, search } from "../controllers/videocontroller";
+import { homepage, search } from "../controllers/videocontroller";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", hompage);
+globalRouter.get("/", homepage);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 globalRouter.get("/search", search);
